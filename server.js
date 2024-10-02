ce39e569d1eba06c3faf5665b003c832bf70d6f6
Reuben <grerlich@gmail.com>
2024-10-02 10:31:13 -0400

    added middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require("./controllers/auth.js");
 const recipesController = require('./controllers/recipes.js');
@@ -30,9 +32,13 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use(passUserToView);
 app.use("/auth", authController);
 app.use('/recipes', recipesController);
 app.use('/ingredients', ingredientsController);
+app.use(isSignedIn);
+app.use("/recipes", recipesController);
+app.use("/ingredients", ingredientsController);
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
