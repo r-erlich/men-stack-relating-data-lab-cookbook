1ee208029b9ef68d562f04bb3b622a060b82e416
Reuben <grerlich@gmail.com>
2024-10-02 10:23:30 -0400

    added recipe and ingredient starter

---
+++ controllers/ingredients.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+const Ingredient = require('../models/ingredient.js');
+
+
+module.exports = router;
---
+++ controllers/recipes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.js");
+const Recipe = require("../models/recipe.js");
+
+
+module.exports = router;
